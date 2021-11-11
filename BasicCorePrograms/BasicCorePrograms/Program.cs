a0553fd887937bbea374746607eb66fe1f0801f0
gunasekhar9 <gunasekhar1539@gmail.com>
2021-11-11 13:07:38 +0530

    [gunasekhar9] add : Vowel or Consonent is Done

--- BasicCorePrograms/BasicCorePrograms/Program.cs
+++ BasicCorePrograms/BasicCorePrograms/Program.cs
@@ -9,7 +9,7 @@ namespace BasicCorePrograms
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter the Program number which is to be executed \n 1. Flip Coin \n 2. Leap Year \n 3. Power of Two \n 4. Harmonic Number \n 5. Factors \n 6. Quotient Remainder \n 7. SwapTwoNumers \n 8. Even Or Odd Number \n 9. Exit");
+                Console.WriteLine("Enter the Program number which is to be executed \n 1. Flip Coin \n 2. Leap Year \n 3. Power of Two \n 4. Harmonic Number \n 5. Factors \n 6. Quotient Remainder \n 7. SwapTwoNumers \n 8. Even Or Odd Number \n 9. Vowel or Consonent \n 10. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -44,6 +44,10 @@ namespace BasicCorePrograms
                         num.EvenOdd();
                         break;
                     case 9:
+                        VowelorConsonant letter = new VowelorConsonant();
+                        letter.Alphabet();
+                        break;
+                    case 10:
                         flag = false;
                         break;
                 }
