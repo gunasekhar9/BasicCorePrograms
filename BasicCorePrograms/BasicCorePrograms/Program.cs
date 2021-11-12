e578b068a0971eecc01222dd43f051e3dbbe26d5
gunasekhar9 <gunasekhar1539@gmail.com>
2021-11-12 15:17:42 +0530

    [gunasekhar] add : Laregest number is Calculated

--- BasicCorePrograms/BasicCorePrograms/Program.cs
+++ BasicCorePrograms/BasicCorePrograms/Program.cs
@@ -9,7 +9,7 @@ namespace BasicCorePrograms
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter the Program number which is to be executed \n 1. Flip Coin \n 2. Leap Year \n 3. Power of Two \n 4. Harmonic Number \n 5. Factors \n 6. Quotient Remainder \n 7. SwapTwoNumers \n 8. Even Or Odd Number \n 9. Vowel or Consonent \n 10. Exit");
+                Console.WriteLine("Enter the Program number which is to be executed \n 1. Flip Coin \n 2. Leap Year \n 3. Power of Two \n 4. Harmonic Number \n 5. Factors \n 6. Quotient Remainder \n 7. SwapTwoNumers \n 8. Even Or Odd Number \n 9. Vowel or Consonent \n 10.Largest Number \n 11.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -48,6 +48,10 @@ namespace BasicCorePrograms
                         letter.Alphabet();
                         break;
                     case 10:
+                        LargestNumber max = new LargestNumber();
+                        max.Largest();
+                        break;
+                    case 11:
                         flag = false;
                         break;
                 }
